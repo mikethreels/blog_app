e3d6bb669d7b3cdc21183d3a9bd6d7d5651c8ad8
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-07-08 12:02:01 -0500

    Added edit, new, destroy and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         @articles = Article.all
     end
@@ -9,5 +11,33 @@ class ArticlesController < ApplicationController
 
     def new
         @article = Article.new
+    end
+
+    def create 
+        @article = Article.new(article_params)
+        @article.save
+        flash.notice = "Article '#{@article.title}' has been created!"
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        @article = Article.find(params[:id]).destroy
+
+        flash.notice = "Article '#{@article.title}' has been deleted!"
+
+        redirect_to articles_path(@articles)
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+      
+        flash.notice = "Article '#{@article.title}' Updated!"
+
+        redirect_to article_path(@article)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
   resources :articles
-
 end
