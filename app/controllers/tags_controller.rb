ca552c8a3352447ec2c8639ea573c2ddfe6eb9fb
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-07-08 15:53:57 -0500

    Added tags functionality

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,18 @@
+class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "The Tag '#{@tag.name}' has been deleted!"
+
+    redirect_to tags_path(@tags)
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :tags
 end
